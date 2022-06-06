6d1711a730b87917cf0f53ba9300af408658d1b9
Christoph Lansche <christoph.lansche@t-online.de>
2022-06-06 16:02:53 +0200

    to-do tracker part 1 with comments

--- todoTrackerServer/app.js
+++ todoTrackerServer/app.js
@@ -1,21 +1,26 @@
-const http = require('http')
-const PORT = process.env.PORT || 5000
-const todos = require("./todos")
+//include http module
+const http = require("http");
+//define PORT on which the server will run either in an .env file or directly
+const PORT = process.env.PORT || 5000;
+//include the content of an endpont as json collection
+const todos = require("./todos");
+//build the server
 const server = http.createServer(function (request, response) {
-    if(request.url === "/api/v1/todos" && request.method === "GET") {
-        response.writeHead(200, {
-            "content-type": "application/json"
-        })
-        response.end(JSON.stringify(todos))
-    }
-})
+  if (request.url === "/api/v1/todos" && request.method === "GET") {
+    response.writeHead(200, {
+      "content-type": "application/json",
+    });
+    response.end(JSON.stringify(todos));
+  }
+});
 
-server.listen(PORT,() => {
-    console.log("Server is ready and running on port ", PORT)
-})
-
+//Make server listen in the defined PORT
+server.listen(PORT, () => {
+  console.log("Server is ready and running on port ", PORT);
+});
+//Error handling
 server.on("error", (error) => {
-    if(error.code = "EADRINUSE") {
-        console.log("Port ", PORT + " is already in use.")
-    }
-})
+  if ((error.code = "EADRINUSE")) {
+    console.log("Port ", PORT + " is already in use.");
+  }
+});
