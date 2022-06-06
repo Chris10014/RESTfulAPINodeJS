7be83131a3f10f6a743e83914e6c7e0231a470cf
Christoph Lansche <christoph.lansche@t-online.de>
2022-06-06 17:53:04 +0200

    to-do tracker part 3b

--- todoTrackerServer/app.js
+++ todoTrackerServer/app.js
@@ -13,13 +13,29 @@ const server = http.createServer(async (request, response) => {
       "content-type": "application/json",
     });
     response.end(JSON.stringify(todos));
-  } else if (request.url === "/api/v1/todos" && request.method === "POST") { // POST requests
+  } else if (request.url === "/api/v1/todos" && request.method === "POST") { // POST requests,  doesn't persist the new todos
     let req_body = await getRequestData(request)
     todos.push(JSON.parse(req_body))
     response.writeHead(201, {
         "content-type": "application/json",
     })
     response.end(JSON.stringify(JSON.parse(req_body)))
+  } else if (request.url.match(/\/api\/v1\/todos\/([0-9])/) && request.method === "DELETE") { // DELETE request
+      const id = request.url.split("/")[4]
+      const todo = todos.find(t => t.id === parseInt(id))
+      if(!todo) {
+          response.writeHead(404, {
+              "content-type": "application/json"
+          })
+          response.end("No todo with id " + id + " found.")
+      } else {
+          const index = todos.indexOf(todo)
+          todos.splice(index, 1)
+          response.writeHead(200, {
+              "content-type": "application/json"
+          })
+          response.end("Deleted todo with id " + id)
+        }
   }
 });
 
